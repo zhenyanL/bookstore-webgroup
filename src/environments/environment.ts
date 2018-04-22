a985cffbc05c7e3b493f6386f28ccb7fd196b1c6
zhenyanL <lin.zheny@husky.neu.edu>
2018-04-22 09:25:28 -0700

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:5000'
+  // baseUrl: 'http://localhost:5000',
+  baseUrl: 'https://bookstore-webgroup.herokuapp.com'
 };
